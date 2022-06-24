6d1920000538049f5090befb2e0f607ebf3e24a4
Your Name <you@example.com>
2022-06-25 01:11:06 +0500

    product image on admin panel

--- users/serializers.py
+++ users/serializers.py
@@ -17,10 +17,7 @@ class CustomTokenObtainPairSerializer(EmailTokenObtainSerializer):
 
     def validate(self, attrs):
         data = super().validate(attrs)
-        refresh = self.get_token(self.user)
 
-        data["refresh"] = str(refresh)
-        data["access"] = str(refresh.access_token)
         data["email"] = self.user.email
         data["first_name"] = self.user.first_name
         data["last_name"] = self.user.last_name
